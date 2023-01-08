0139bd6f3f9e6fe4107f4acae51511dfe5c02457
sumit Qureshi <cosmicdanish@outlook.com>
2023-01-08 17:27:33 +0530

    08-01-2023

--- src/remVowel1.java
+++ src/remVowel1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Q1RemoveVowel {
+public class remVowel1 {
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
        String str = sc.next();
